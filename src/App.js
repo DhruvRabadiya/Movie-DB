5f66a321a813435c31378e66e7a3aba37a885d87
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-03-30 18:36:22 +0530

    creating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//components
+import Header from "./components/Header";
 
 //styles
 import { GloBalStyle } from "./GlobalStyle";
 function App() {
   return (
     <div className="App">
+        <Header/>
       Start Here.
       <GloBalStyle />
     </div>
