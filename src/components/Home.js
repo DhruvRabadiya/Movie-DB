f84fd928c458d6c50e7206c658fc85d08066c120
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-03-31 14:39:35 +0530

    movie data loaded

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+//API
+import API from "../API";
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
@@ -7,7 +10,8 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //hook
 
 //Image
-import NoImage from '../images/no_image.jpg`';
+import NoImage from "../images/no_image.jpg";
+import { Form } from "react-router-dom";
 
 const Home = () => {
   const [state, setState] = useState();
@@ -15,6 +19,33 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+
+setState(prev => ({
+  ...movies,
+  results:
+    page > 1 ?[...prev.results, ...movies.results] :[...movies.results]
+  
+}))
+
+    } catch (error) {
+      setError(true);
+    }
+
+    setLoading(false)
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  console.log(state)
   return <div>Home Page</div>;
 };
 
