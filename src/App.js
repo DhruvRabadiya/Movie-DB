31016abe5526b4fd6569433258c194d6d76d5a0b
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-03-31 12:57:44 +0530

    coponents created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 //components
 import Header from "./components/Header";
-
+import Home from "./components/Home";
 //styles
 import { GloBalStyle } from "./GlobalStyle";
 function App() {
   return (
     <div className="App">
         <Header/>
-      Start Here.
+      <Home/>
       <GloBalStyle />
     </div>
   );
