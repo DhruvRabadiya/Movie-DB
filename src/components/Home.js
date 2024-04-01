eef37a7f5ce1558d688f46a1b2ebd089d98db9b9
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-04-01 23:39:00 +0530

    making hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 
-
-
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
-
+import HeroImage from "./HeroImage";
 //hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 //Image
@@ -13,9 +11,19 @@ import NoImage from "../images/no_image.jpg";
 import { Form } from "react-router-dom";
 
 const Home = () => {
-  const {state , loading , error} = useHomeFetch( )
-  console.log(state)
-  return <div>Home Page</div>;
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
