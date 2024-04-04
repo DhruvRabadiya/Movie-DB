77bf53c73a66a46cf4dc54e542eddb1e4e97f810
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-04-04 12:01:31 +0530

    movie data fetch

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 //config
-import { IMAGE_BASE_URL , POSTER_SIZE } from "../config";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 //components
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 
 //Hooks
-
+import {useMovieFetch} from "../hooks/useMovieFetch";
 
 //Image
 
-import NoImage from '../images/no_image.jpg'
+import NoImage from "../images/no_image.jpg";
 const Movie = () => {
 
-    return (
-        
-        <>
-        <div>Movie</div>
-        </>
-    )
-}
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  console.log(movie);
 
-export default Movie
+
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
+
+export default Movie;
