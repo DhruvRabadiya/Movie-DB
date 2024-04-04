5028b429597b4de2e5a3ec9518d30eeb2d10b756
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-04-04 13:08:44 +0530

    movie Page Created

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,9 +2,13 @@ import React from "react";
 import { useParams } from "react-router-dom";
 //config
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
 //components
+
+import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
+import MovieInfo from "./MovieInfo";
 
 //Hooks
 import {useMovieFetch} from "../hooks/useMovieFetch";
@@ -16,12 +20,14 @@ const Movie = () => {
 
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
-  console.log(movie);
 
+  if(loading) return <Spinner/>
+  if(error) return <div>Something went wrong...</div>
 
   return (
     <>
-      <div>Movie</div>
+    <BreadCrumb movieTitle={movie.original_title}/>
+    <MovieInfo movie={movie} /> 
     </>
   );
 };
