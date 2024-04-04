4cf971dfda0c3a3ecc1f9f38cb9009ad346e3c0e
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-04-04 16:04:16 +0530

    Movie Info. Bar Created

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,25 +9,30 @@ import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 
 //Hooks
-import {useMovieFetch} from "../hooks/useMovieFetch";
+import { useMovieFetch } from "../hooks/useMovieFetch";
 
 //Image
 
 import NoImage from "../images/no_image.jpg";
 const Movie = () => {
-
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  if(loading) return <Spinner/>
-  if(error) return <div>Something went wrong...</div>
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
 
   return (
     <>
-    <BreadCrumb movieTitle={movie.original_title}/>
-    <MovieInfo movie={movie} /> 
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
     </>
   );
 };
